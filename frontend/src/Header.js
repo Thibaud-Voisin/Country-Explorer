f8d68eed9bf3c3eae366d08f966a4c0fd11c6015
Thibaud Voisin <thibaud.voisin@epita.fr>
2023-11-30 14:26:33 +0100

    Cypress testing v2

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -24,7 +24,7 @@ const Header = ({ toggleTheme, isVisibleAlert }) => {
   <ul className="m-3 p-1 menu menu-horizontal bg-base-200 rounded-box">
   <li className='m-1'>
   <label className="p-2 swap swap-rotate">
-      <svg onClick={scrollUp} xmlns="http://www.w3.org/2000/svg" className="w-[6vw] h-[6vw] xl:h-[2vw] xl:w-[2vw]" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
+      <svg onClick={scrollUp} xmlns="http://www.w3.org/2000/svg" className="home_btn w-[6vw] h-[6vw] xl:h-[2vw] xl:w-[2vw]" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
     </label>
   </li>
   <li className='m-1'>
