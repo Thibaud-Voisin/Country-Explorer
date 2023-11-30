fe39e30d94cd5a2bb7de39e761ec82b8a1c7029d
Thibaud Voisin <thibaud.voisin@epita.fr>
2023-11-30 13:21:28 +0100

    carousel change

--- frontend/cypress/e2e/fullTesting.cy.js
+++ frontend/cypress/e2e/fullTesting.cy.js
@@ -1,6 +1,7 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://country-explorer-lovat.vercel.app')
-    cy.get('.selectCountry').click();
+    cy.get('.selectCountry').click()
+    cy.get('.selectCountry').type('Ireland')
   })
 })
--- frontend/src/Result/Carousel.js
+++ frontend/src/Result/Carousel.js
@@ -2,42 +2,35 @@ import { React } from 'react'
 import propTypes from 'prop-types'
 
 const Carousel = ({ images }) => {
-  // Function to get to a specific image
-  function goToSlide (slideId) {
-    const slideElement = document.querySelector(slideId)
-    if (slideElement) {
-      slideElement.scrollIntoView({ behavior: 'smooth' })
-    }
-  }
   // Rendering the carousel
   return (
     <div className="carousel rounded-2xl w-full h-full">
         <div id="slide1" className="carousel-item relative w-full">
             <img src={images[0]} className="inset-0 w-full h-full object-cover" />
             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                <button onClick={() => goToSlide('#slide4')} className="btn btn-circle">❮</button>
-                <button onClick={() => goToSlide('#slide2')} className="btn btn-circle">❯</button>
+            <a href="#slide4" className="btn btn-circle">❮</a>
+      <a href="#slide2" className="btn btn-circle">❯</a>
             </div>
         </div>
         <div id="slide2" className="carousel-item relative w-full">
             <img src={images[1]} className="inset-0 w-full h-full object-cover" />
             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                <button onClick={() => goToSlide('#slide1')} className="btn btn-circle">❮</button>
-                <button onClick={() => goToSlide('#slide3')} className="btn btn-circle">❯</button>
+            <a href="#slide1" className="btn btn-circle">❮</a>
+      <a href="#slide3" className="btn btn-circle">❯</a>
             </div>
         </div>
         <div id="slide3" className="carousel-item relative w-full">
             <img src={images[2]} className="inset-0 w-full h-full object-cover" />
             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                <button onClick={() => goToSlide('#slide2')} className="btn btn-circle">❮</button>
-                <button onClick={() => goToSlide('#slide4')} className="btn btn-circle">❯</button>
+            <a href="#slide2" className="btn btn-circle">❮</a>
+      <a href="#slide4" className="btn btn-circle">❯</a>
             </div>
         </div>
         <div id="slide4" className="carousel-item relative w-full">
             <img src={images[3]} className="inset-0 w-full h-full object-cover" />
             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                <button onClick={() => goToSlide('#slide3')} className="btn btn-circle">❮</button>
-                <button onClick={() => goToSlide('#slide1')} className="btn btn-circle">❯</button>
+            <a href="#slide3" className="btn btn-circle">❮</a>
+      <a href="#slide1" className="btn btn-circle">❯</a>
             </div>
         </div>
     </div>
